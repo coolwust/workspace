af7387c230e47826c4f1977ffcd87c07734dbba3
coolwust <coolwust@gmail.com>
2017-02-02 21:46:32 +0000

    update

--- fedora/root.sh
+++ fedora/root.sh
@@ -20,7 +20,7 @@ dnf -y update && dnf -y install \
 	mktemp \
 	xdg-user-dirs
 
-# node and typescript
+# Node and TypeScript
 NODE_VERSION=v7.4.0
 if ! command -v node; then
 	curl -fsSL "https://nodejs.org/dist/${NODE_VERSION}/node-${NODE_VERSION}-linux-x64.tar.xz" | tar -C /opt -Jxf -
@@ -32,7 +32,7 @@ if ! command -v tsc; then
 	npm install -g typescript
 fi
 
-# go
+# Go
 GO_VERSION=1.7.4
 if ! command -v go; then
 	curl -fsSL "https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz" | tar -C /opt -xzf -
@@ -42,30 +42,44 @@ if ! command -v go; then
 EOF
 fi
 
-# gcloud
-GCLOUD_VERSION=141.0.0
+# Google Cloud SDK
+GOOGLE_CLOUD_SDK_VERSION=141.0.0
 if ! command -v gcloud; then
 	dnf -y install python
-	curl -fsSL "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${GCLOUD_VERSION}-linux-x86_64.tar.gz" | tar -C /opt -xzf -
-	printf 'export PATH=$PATH:/opt/google-cloud-sdk/bin' >/etc/profile.d/gcloud.sh
+	curl -fsSL "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${GOOGLE_CLOUD_SDK_VERSION}-linux-x86_64.tar.gz" | tar -C /opt -xzf -
+	printf 'export PATH=$PATH:/opt/google-cloud-sdk/bin' >/etc/profile.d/google-cloud-sdk.sh
 fi
 
-# go appengine
-GO_APPENGINE_VERSION=1.9.48
+# Google Go App Engine SDK
+GOOGLE_GO_APP_ENGINE_SDK_VERSION=1.9.48
 if ! command -v goapp; then
-	dnf -y install python
+	dnf -y install \
+		python \
+		mktemp
 	tmp=$(mktemp)
-	curl -o $tmp -fsSL "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-${GO_APPENGINE_VERSION}.zip"
+	curl -o $tmp -fsSL "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-${GOOGLE_GO_APP_ENGINE_SDK_VERSION}.zip"
 	7za x -o/opt -tzip $tmp
 	rm $tmp
 	printf 'export PATH=$PATH:/opt/go_appengine' >/etc/profile.d/go_appengine.sh
 fi
 
-# docker
+# Docker
 DOCKER_VERSION=1.13.0
 if ! command -v docker; then
 	dnf -y install dnf-plugins-core
 	dnf config-manager --add-repo https://docs.docker.com/engine/installation/linux/repo_files/fedora/docker.repo
 	dnf makecache fast
 	dnf -y install docker-engine
+fi
+
+# Protocol Buffers
+PROTOCOL_BUFFERS_VERSION=3.0.0
+if ! command -v protoc; then
+	dnf -y install mktemp
+	tmp=$(mktemp)
+	curl -o $tmp -fsSL "https://github.com/google/protobuf/releases/download/v${PROTOCOL_BUFFERS_VERSION}/protoc-${PROTOCOL_BUFFERS_VERSION}-linux-x86_64.zip"
+	mkdir /opt/protobuf
+	7za x -o/opt/protobuf -tzip $tmp
+	rm $tmp
+	printf 'export PATH=$PATH:/opt/protobuf/bin' >/etc/profile.d/protobuf.sh
 fi
--- fedora/user.sh
+++ fedora/user.sh
@@ -7,35 +7,46 @@ mkdir -p ~/bin
 mkdir -p ~/.config/systemd/user
 printf "export TERM=xterm-256color\n" >>~/.bash_profile
 
-# git 
+# Git
 mkdir -p ~/.config/git
 cp git/gitignore ~/.config/git/ignore
 
-# vim
+# Vim
 cp vim/vimrc ~/.vimrc
 git clone "https://github.com/fatih/vim-go.git" ~/.vim/pack/plugins/start/vim-go
 git clone "https://github.com/leafgarland/typescript-vim.git" ~/.vim/pack/plugins/start/typescript-vim
 git clone "https://github.com/vim-scripts/DrawIt.git" ~/.vim/pack/plugins/start/DrawIt
 
-# tmux
+# Tmux
 cp tmux/tmux.conf ~/.tmux.conf
 
-# ranger
+# Ranger
 mkdir -p ~/.config/ranger
 cp ranger/rc.conf ~/.config/ranger/rc.conf
 
-# go
+# Go
 mkdir -p ~/Documents/go/src ~/Documents/go/bin
 cat <<-'EOF' >>~/.bash_profile
 	export GOPATH=$HOME/Documents/go
 	export PATH=$PATH:$GOBIN
 EOF
 
-# c
+# C
 mkdir -p ~/Documents/c
 
-# dropbox
+# Dropbox
 curl -fsSL "https://www.dropbox.com/download?plat=lnx.x86_64" | tar -C ~ -zxf -
 curl -o ~/bin/dropbox -fsSL "https://www.dropbox.com/download?dl=packages/dropbox.py"
 chmod u+x ~/bin/dropbox
 cp dropbox/dropbox.service ~/.config/systemd/user
+
+# Go support for Protocol Buffers
+# This software has two parts: the protoc-gen-go is a 'protocol compiler plugin'
+# that generates Go # source files that, once compiled, can access and manage
+# protocol buffers; and the proto package is a library that implements run-time
+# support for encoding (marshaling), decoding (unmarshaling), and accessing
+# protocol buffers.
+go get -u github.com/golang/protobuf/{protoc-gen-go, proto}
+
+# gRPC
+go get google.golang.org/grpc
