6c724e0d972055469e52fcabea367cc0009d5ea6
coolwust <coolwust@gmail.com>
2017-02-03 08:16:53 +0000

    update

--- fedora/root/install.sh
+++ fedora/root/install.sh
@@ -27,14 +27,14 @@ NODE_VERSION=v7.4.0
 curl -fsSL "https://nodejs.org/dist/${NODE_VERSION}/node-${NODE_VERSION}-linux-x64.tar.xz" | tar -C /opt -Jxf -
 mv /opt/node-${NODE_VERSION}-linux-x64 /opt/nodejs
 cp $SCRIPT_DIR/nodejs/profile.sh /etc/profile.d/nodejs.sh
-. /etc/profile.d/nodejs.sh
+. $SCRIPT_DIR/nodejs/profile.sh
 npm install -g typescript
 
 # Go
 GO_VERSION=1.7.4
 curl -fsSL "https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz" | tar -C /opt -xzf -
 cp $SCRIPT_DIR/go/profile.sh /etc/profile.d/go.sh
-. /etc/profile.d/go.sh
+. $SCRIPT_DIR/go/profile.sh
 
 # Google Cloud SDK
 GOOGLE_CLOUD_SDK_VERSION=141.0.0
@@ -42,7 +42,7 @@ dnf -y install python
 curl -fsSL "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${GOOGLE_CLOUD_SDK_VERSION}-linux-x86_64.tar.gz" | tar -C /opt -xzf -
 mv /opt/google-cloud-sdk /opt/google_cloud_sdk
 cp $SCRIPT_DIR/google_cloud_sdk/profile.sh /etc/profile.d/google_cloud_sdk.sh
-. /etc/profile.d/google_cloud_sdk.sh
+. $SCRIPT_DIR/google_cloud_sdk/profile.sh
 
 # Go App Engine SDK
 GO_APP_ENGINE_SDK_VERSION=1.9.48
@@ -52,7 +52,7 @@ curl -o /opt/go_app_engine_sdk.zip -fsSL "https://storage.googleapis.com/appengi
 rm /opt/go_app_engine_sdk.zip
 mv /opt/go_appengine /opt/go_app_engine_sdk
 cp $SCRIPT_DIR/go_app_engine_sdk/profile.sh /etc/profile.d/go_app_engine_sdk.sh
-. /etc/profile.d/go_app_engine_sdk.sh
+. $SCRIPT_DIR/google_cloud_sdk/profile.sh
 
 # Protocol Buffers
 PROTOCOL_BUFFERS_VERSION=3.0.0
@@ -61,7 +61,7 @@ mkdir /opt/protocol_buffers
 7za x -o/opt/protocol_buffers -tzip /opt/protocol_buffers.zip
 rm /opt/protocol_buffers.zip
 cp $SCRIPT_DIR/protocol_buffers/profile.sh /etc/profile.d/protocol_buffers.sh
-. /etc/profile.d/protocol_buffers.sh
+. $SCRIPT_DIR/protocol_buffers/profile.sh
 
 # Docker
 DOCKER_VERSION=1.13.0
