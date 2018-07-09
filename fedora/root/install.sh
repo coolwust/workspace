a459164080475240e9bfac0da938a1756d95ebad
Xianghan Wang <coolwust@gmail.com>
2018-07-09 23:45:21 +0000

    update

--- fedora/root/install.sh
+++ fedora/root/install.sh
@@ -12,7 +12,7 @@ dnf -y update && dnf -y install \
 	man \
 	mktemp \
 	p7zip \
-	python \
+	python3 \
 	ranger \
 	tar \
 	tmux \
@@ -83,25 +83,13 @@ mv $TEMPDIR/google-cloud-sdk /opt/google_cloud_sdk
 echo 'export PATH=$PATH:/opt/google_cloud_sdk/bin' >/etc/profile.d/google_cloud_sdk.sh
 
 # Protocol Buffers
-PROTOCOL_BUFFERS_VERSION=3.3.0
-curl -o $TEMPDIR/protocol_buffers.zip -fsSL "https://github.com/google/protobuf/releases/download/v${PROTOCOL_BUFFERS_VERSION}/protoc-${PROTOCOL_BUFFERS_VERSION}-linux-x86_64.zip"
-mkdir /opt/protocol_buffers
-7za x -o/opt/protocol_buffers -tzip $TEMPDIR/protocol_buffers.zip
-find /opt/protocol_buffers -type f -exec chmod 664 {} \+
-find /opt/protocol_buffers -type d -exec chmod 775 {} \+
-chmod a+x /opt/protocol_buffers/bin/*
-echo 'export PATH=$PATH:/opt/protocol_buffers/bin' >/etc/profile.d/protocol_buffers.sh
-
-# Docker
-dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-dnf makecache fast
-dnf -y install docker-ce
-
-# Docker Compose
-DOCKER_COMPOSE_VERSION=1.13.0
-mkdir -p /opt/docker_compose
-curl -fsSL -o /opt/docker_compose/docker-compose https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-Linux-x86_64
-chmod a+x /opt/docker_compose/docker-compose
-echo 'export PATH=$PATH:/opt/docker_compose' >/etc/profile.d/docker_compose.sh
+PROTOCOL_BUFFERS_VERSION=3.6.0
+curl -o $TEMPDIR/protoc.zip -fsSL "https://github.com/google/protobuf/releases/download/v${PROTOCOL_BUFFERS_VERSION}/protoc-${PROTOCOL_BUFFERS_VERSION}-linux-x86_64.zip"
+mkdir /opt/protoc
+7za x -o/opt/protoc -tzip $TEMPDIR/protoc.zip
+echo 'export PATH=$PATH:/opt/protoc/bin' >/etc/profile.d/protoc.sh
 
 rm -Rf $TEMPDIR
+find /opt -type f -exec chmod 664 {} \+
+find /opt -type d -exec chmod 775 {} \+
+chmod a+x /opt/go/bin/* /opt/protoc/bin/*
